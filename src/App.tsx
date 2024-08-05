8b43b94cad72affcfc8be0d395cc733c31767f62
adhinnnugroho <adhinnnugroho@gmail.com>
2024-08-05 17:25:08 +0700

    feat: setup mobile navigations

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import './App.css'
-import MainLayout from '@layouts/MainLayouts'
+import MainLayout from './layouts/mainLayouts'
+import MobileNavigations from './layouts/mobileNavigations'
 
 function App() {
 
   return (
     <MainLayout>
-      test hanya est
+      <div className="p-3">
+        <h5 className="text-xl">
+          Hai, Selamat Datang
+        </h5>
+        <h3>
+          Nama Pengguna
+        </h3>
+      </div>
+
+      <MobileNavigations />
     </MainLayout>
   )
 }
---
+++ src/Layouts/mobileNavigations.tsx
@@ -0,0 +1,27 @@
+
+
+const MobileNavigations = () => {
+    return (
+        <div className="fixed w-screen bottom-0 bg-gray-300 p-3">
+            <div className="grid grid-cols-4 gap-5">
+                <div className="col-span-1 flex justify-center">
+                    <i className='bx bx-home-alt  text-2xl text-blue-400 font-semibold' />
+                    <span className="ml-2 mt-1 text-blue-500 font-semibold">
+                        Home
+                    </span>
+                </div>
+                <div className="col-span-1 flex justify-center">
+                    <i className='bx bx-calendar text-2xl' />
+                </div>
+                <div className="col-span-1 flex justify-center">
+                    <i className='bx bx-heart text-2xl' />
+                </div>
+                <div className="col-span-1 flex justify-center">
+                    <i className='bx bx-user text-2xl' />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default MobileNavigations
